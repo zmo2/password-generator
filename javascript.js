996dd8cb3d1e97dc5cb4ee440c46a41e594e48f5
ink <eric.zmo2@gmail.com>
2020-01-17 21:35:37 -0800

    initial logic for selected options

--- javascript.js
+++ javascript.js
@@ -1,21 +1,46 @@
-var lowerAlp = "abcdefghijk"
-var upperAlp = "ABCDEFGHIJK"
+var lowerAlp = "abcdefghijklmnopqrstuvwxyz"
+var upperAlp = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
 var specialSym = "!@#$%^&*"
 var numbers = "1234567890"
 var finalPassword = "";
+var optionList = document.getElementById("passOptions")
+var selectedItems = optionList.selectedOptions
+var wantSymbol = false
+var wantLower = false
+var wantUpper = false
+var wantNumber =false
+
+
 
 function clearPassword() {
     document.getElementById('mainDisplay').innerText = "Working";
     finalPassword = "";
+    getSelectedItems()
+}
+
+function getSelectedItems(){
+    for (i=0; i<selectedItems.length; i++){
+        if (selectedItems[i].label === "LowerCase"){
+            wantLower = true
+        }if (selectedItems[i].label === "UpperCase"){
+            wantUpper = true
+        }if (selectedItems[i].label === "SpecialSymbols"){
+            wantSymbol = true
+        }if (selectedItems[i].label === "Numbers"){
+            wantNumber = true
+        }
+        console.log(selectedItems[i].label)
+    }
 }
 
 function generatePassword(){
     var passwordLength = document.getElementById("passLength").value;
         for (i=0; i<passwordLength; i++){
-            console.log(finalPassword.concat(lowerAlp.charAt(Math.floor(Math.random() * lowerAlp.length))))
-            console.log(lowerAlp.length)
+            
             finalPassword = finalPassword.concat(lowerAlp.charAt(Math.floor(Math.random() * lowerAlp.length)));
         }
+
+
 
     document.getElementById('mainDisplay').innerText = finalPassword;
 }
